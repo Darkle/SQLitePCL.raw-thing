0f75ab899d304101932b90ae2de800d0c9a35f03
Coop <coop.coding@gmail.com>
2024-07-09 16:03:06 +1000

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿open Microsoft.Data.Sqlite
+open SQLitePCL
+
+
+[<Core.EntryPointAttribute>]
+let main _ =
+    printfn "running"
+    let sqlConString = new SqliteConnectionStringBuilder()
+    sqlConString.DataSource <- "./foo.db"
+
+    let sqLiteConnection = new SqliteConnection(sqlConString.ConnectionString)
+    sqLiteConnection.Open()
+
+    let conn = sqLiteConnection.Handle
+
+    SQLitePCL.raw.sqlite3_trace (conn, delegate_trace (fun _ _ -> ()), null)
+    0
